7c430b3cf5435a42bf7ea4de8a031aaff6ee8e94
Chris Cankov <cankov.chris@gmail.com>
2017-04-11 15:54:31 -0700

    setup database connection and question class

---
+++ questions.rb
@@ -0,0 +1,107 @@
+require 'sqlite3'
+require 'singleton'
+require_relative 'model_base.rb'
+require_relative 'users.rb'
+require_relative 'question_follows.rb'
+require_relative 'replies.rb'
+require_relative 'question_likes.rb'
+
+class QuestionDBConnection < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Question
+  @@table = 'questions'
+
+  attr_accessor :title, :body, :author_id
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def self.find_by_id(id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+    Question.new(data.first)
+  end
+
+  def self.find_by_author_id(author_id)
+    data = QuestionDBConnection.instance.execute(<<-SQL, author_id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        author_id = ?
+    SQL
+    data.map{|datum| Question.new(datum)}
+  end
+
+  def self.most_likes(n)
+    QuestionLike.most_liked_questions(n)
+  end
+
+  def author
+    User.find_by_id(@author_id)
+  end
+
+  def replies
+    Reply.find_by_question_id(@id)
+  end
+
+  def followers
+    QuestionFollow.followers_for_question_id(@id)
+  end
+
+  def likers
+    QuestionLike.likers_for_question_id(@id)
+  end
+
+  def num_likes
+    QuestionLike.num_likes_for_question_id(@id)
+  end
+
+  def save
+    @id ? update : create
+  end
+
+  private
+
+  def create
+    raise "#{self} already in database" if @id
+    QuestionDBConnection.instance.execute(<<-SQL, @title, @body, @author_id)
+      INSERT INTO
+        questions (title, body, author_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    @id = QuestionDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    QuestionDBConnection.instance.execute(<<-SQL, @title, @body, @author_id, @id)
+      UPDATE
+        questions
+      SET
+        title = ?, body = ?, author_id = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
